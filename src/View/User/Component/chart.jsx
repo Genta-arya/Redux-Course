9c27ccab4c08931509b662f3e050eb709201b1b8
GentaArya <mgentaarya@yahoo.com>
2023-12-14 01:14:27 +0700

    add framer motion

--- src/View/User/Component/chart.jsx
+++ src/View/User/Component/chart.jsx
@@ -50,13 +50,13 @@ const CartModalContent = ({
     });
   };
 
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (modalRef.current && !modalRef.current.contains(event.target)) {
-        closeCartModal();
-      }
-    };
+  const handleClickOutside = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      closeCartModal();
+    }
+  };
 
+  useEffect(() => {
     document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
@@ -168,11 +168,12 @@ const CartModalContent = ({
         animate={{ opacity: 1, scale: 1 }}
         exit={{ opacity: 0, scale: 0.8 }}
         transition={{ duration: 0.5 }}
+        ref={modalRef}
         className={`bg-white p-6 rounded-md shadow-lg relative ${styles.cartContainer} h-[500px] overflow-y-auto  w-[90%]`}
       >
-       <motion.button
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
           className="absolute top-2 left-4 text-gray-500 hover:text-gray-700 focus:outline-none"
           onClick={closeCartModal}
         >
@@ -185,7 +186,6 @@ const CartModalContent = ({
         ) : (
           cartItems.map((item) => (
             <motion.div
-           
               className={`mb-4 flex items-center justify-between border-b border-gray-300 pb-2 ${
                 itemToRemove === item.id ? styles.slideOut : ""
               }`}
