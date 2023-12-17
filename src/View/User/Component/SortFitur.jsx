3dc33c4880b4824a37d7b8bf66ff228fe91a3617
GentaArya <mgentaarya@yahoo.com>
2023-12-17 10:53:18 +0700

    check sorting

--- src/View/User/Component/SortFitur.jsx
+++ src/View/User/Component/SortFitur.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFilter, faSort } from "@fortawesome/free-solid-svg-icons";
+import { faSort } from "@fortawesome/free-solid-svg-icons";
 import { selectCharter, setCharter } from "./productlist/fitur/productSlice";
 
 const SortFitur = () => {
@@ -9,45 +9,47 @@ const SortFitur = () => {
   const charter = useSelector(selectCharter);
 
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [sortText, setSortText] = useState("Sort");
 
-  const handleCharterChange = (newCharter) => {
+  const handleCharterChange = (newCharter, newText) => {
     dispatch(setCharter(newCharter));
+    setSortText(newText);
     setDropdownOpen(false);
   };
 
   return (
-    <div className="flex justify-center gap-4 relative mt-4  ">
+    <div className="flex justify-center gap-4 relative mt-4">
       <div className="relative mx-auto w-[90%]">
         <div
           onClick={() => setDropdownOpen(!dropdownOpen)}
           className="cursor-pointer px-2 py-1 border rounded    flex items-center justify-between"
         >
-          <div className="text-white">sort</div>
+          <div className="text-white">{sortText}</div>
           <FontAwesomeIcon icon={faSort} className="ml-2 text-white" />
         </div>
 
         {dropdownOpen && (
           <div className="absolute mt-2 w-[100%] lg:w-[100%] md:w-[100%] bg-white border rounded-md shadow-lg z-50">
             <div
-              onClick={() => handleCharterChange("high")}
+              onClick={() => handleCharterChange("high", "A-Z")}
               className="cursor-pointer py-2 px-4 hover:bg-gray-100 text-xs"
             >
               A-Z
             </div>
             <div
-              onClick={() => handleCharterChange("low")}
+              onClick={() => handleCharterChange("low", "Z-A")}
               className="cursor-pointer py-2 px-4 hover:bg-gray-100 text-xs"
             >
               Z-A
             </div>
             <div
-              onClick={() => handleCharterChange("lowPrice")}
+              onClick={() => handleCharterChange("lowPrice", "Price (high to low)")}
               className="cursor-pointer py-2 px-4 hover:bg-gray-100 text-xs"
             >
               Price (high to low)
             </div>
             <div
-              onClick={() => handleCharterChange("highPrice")}
+              onClick={() => handleCharterChange("highPrice", "Price (low to high)")}
               className="cursor-pointer py-2 px-4 hover:bg-gray-100 text-xs"
             >
               Price (low to high)
