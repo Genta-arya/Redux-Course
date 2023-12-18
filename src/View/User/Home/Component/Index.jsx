f1faacba6ad1b9464e5139a59528305298e26882
GentaArya <mgentaarya@yahoo.com>
2023-12-18 09:55:33 +0700

    update seo

--- src/View/User/Home/Component/Index.jsx
+++ src/View/User/Home/Component/Index.jsx
@@ -5,27 +5,10 @@ import Content from "./Content";
 import FooterDaisy from "../../Component/FooterDaisy";
 import Footer from "../../Component/Footer";
 import CustomerReviews from "./CustomerReview";
-import image from "../../../../assets/seo.png"
+import image from "../../../../assets/seo.png";
 const IndexLp = () => {
   return (
     <main className="bg-white">
-      <Helmet>
-        <title>HKKS SHOP</title>
-        <meta
-          name="description"
-          content="Welcome to HKKS SHOP"
-        />
-        <meta property="og:title" content="HKKS SHOP" />
-        <meta
-          property="og:description"
-          content="Welcome to HKKS SHOP"
-        />
-        <meta
-          name="og:image"
-          content={`https://www.hkks.shop` + image}
-        />
-      </Helmet>
-
       <Header />
       <Content />
       <CustomerReviews />
